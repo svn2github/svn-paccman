1b1e345267eac25e0e8a797361f6889d8933500e
joaof <joaof@c52a3114-37a6-436a-893b-6c6ca14ccb13>
2007-09-20 08:22:01 +0000

    git-svn-id: https://svn.code.sf.net/p/paccman/code@94 c52a3114-37a6-436a-893b-6c6ca14ccb13

--- paccman-ui-common/src/org/paccman/ui/common/AccountComboBox.java
+++ paccman-ui-common/src/org/paccman/ui/common/AccountComboBox.java
@@ -1,49 +1,45 @@
 /*
- 
-    Copyright (C)    2005 Joao F. (joaof@sourceforge.net)
-                     http://paccman.sourceforge.net 
-
-    This program is free software; you can redistribute it and/or modify      
-    it under the terms of the GNU General Public License as published by      
-    the Free Software Foundation; either version 2 of the License, or         
-    (at your option) any later version.                                       
-
-    This program is distributed in the hope that it will be useful,           
-    but WITHOUT ANY WARRANTY; without even the implied warranty of            
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             
-    GNU General Public License for more details.                              
-
-    You should have received a copy of the GNU General Public License         
-    along with this program; if not, write to the Free Software               
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
- 
-*/
+Copyright (C)    2005 Joao F. (joaof@sourceforge.net)
+http://paccman.sourceforge.net
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 package org.paccman.ui.common;
-import org.paccman.controller.PaccmanView;
-//:TODO: import org.paccman.ui.main.Main;
 
+import org.paccman.controller.PaccmanView;
+
+//:TODO: import org.paccman.ui.main.Main;
 /**
  *
  * @author joao
  */
 public class AccountComboBox extends PaccmanObjectComboBox implements PaccmanView {
-    
+
     /** Creates a new instance of BankComboBox */
     public AccountComboBox() {
         setModel(new AccountComboModel());
     }
-    
+
+    @Override
     public void onChange(org.paccman.controller.Controller controller) {
 /*:TODO:        if (controller == Main.getDocumentCtrl()) {
-            removeAllItems();
-            addItem(null);
-            for (Account account: Main.getDocumentCtrl().getDocument().getAccounts()) {
-                AccountController accountCtrl = (AccountController)ControllerManager.getController(account);
-                addItem(accountCtrl);
-            }
+        removeAllItems();
+        addItem(null);
+        for (Account account: Main.getDocumentCtrl().getDocument().getAccounts()) {
+        AccountController accountCtrl = (AccountController)ControllerManager.getController(account);
+        addItem(accountCtrl);
         }
- */
+        }
+         */
     }
-    
 }
