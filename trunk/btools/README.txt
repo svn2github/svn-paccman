594bf7ca7001c97b8f6641ca41bd8af68f3c1315
joaof <joaof@c52a3114-37a6-436a-893b-6c6ca14ccb13>
2008-07-13 22:12:58 +0000

    Delete all. Restart from scratch using the latest technologies now :-)
    git-svn-id: https://svn.code.sf.net/p/paccman/code@231 c52a3114-37a6-436a-893b-6c6ca14ccb13

--- trunk/btools/README.txt
+++
@@ -1,3 +0,0 @@
-This directory is for scripts and tool sources used in the PAccMan project (btools stands dor build tools)
-
-
--- trunk/paccman-db/sql-scripts/initialize.sql
+++
@@ -1,9 +0,0 @@
---
---    Initialization of default data in database for PAccMan.
---
-
-INSERT INTO "APP"."REF_COUNTRIES" (NAME) VALUES ('France');
-INSERT INTO "APP"."REF_COUNTRIES" (NAME) VALUES ('Germany');
-INSERT INTO "APP"."REF_COUNTRIES" (NAME) VALUES ('Portugal');
-INSERT INTO "APP"."REF_COUNTRIES" (NAME) VALUES ('Spain');
-INSERT INTO "APP"."REF_COUNTRIES" (NAME) VALUES ('United Kingdom');
