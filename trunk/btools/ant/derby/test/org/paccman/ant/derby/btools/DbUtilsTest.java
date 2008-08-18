eb76147579a0ceaeebbd0fbdf6f9314ae4efa0d8
joaof <joaof@c52a3114-37a6-436a-893b-6c6ca14ccb13>
2008-08-18 07:20:21 +0000

    Initial import for btools-ant-derby (cont'ed)
    git-svn-id: https://svn.code.sf.net/p/paccman/code@243 c52a3114-37a6-436a-893b-6c6ca14ccb13

---
+++ trunk/btools/ant/derby/test/org/paccman/ant/derby/btools/DbUtilsTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.paccman.ant.derby.btools;
+
+import org.paccman.btools.ant.derby.DerbyUtils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author joao
+ */
+public class DbUtilsTest {
+
+    public DbUtilsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of shutdownDb method, of class DerbyUtils.
+     * @throws java.lang.Exception 
+     */
+    @Test
+    public void shutdownDb() throws Exception {
+        System.out.println("shutdownDb");
+        Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        String connectionString = "jdbc:derby:/home/joao/data/devel/projects/paccman/src/paccman-db/build/template.paccmandb";
+        Connection connection = DriverManager.getConnection(connectionString);
+        connection.close();
+        DerbyUtils.shutdownDb(connectionString);
+    }
+}
